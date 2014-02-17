3cdc63906e02f1773fce0c3b69bd4697a843b80c
Nag <Nag@Nag-PC>
2014-02-17 17:01:14 +0530

    erpjsf

---
+++ src/main/java/com/bean/Student.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package com.bean;
+
+/**
+ * @author Nag
+ *
+ */
+public class Student {
+	private String name;
+	private String password;
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+	/**
+	 * @param password the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+
+}
